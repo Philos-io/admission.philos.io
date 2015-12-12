dc1d5153dc41446da2f9215d9471ac93c7fbcbc3
davy engone <davy@philos.io>
2015-12-12 17:27:23 +0100

    express configuration

---
+++ server/express.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let path = require('path');
+let express = require('express');
+let passport = require('passport');
+let	logger = require('morgan');
+let session = require('express-session');
+let methodOverride = require('method-override');
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+
+
+module.exports = function(app, passport){
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(session({
+    secret: 'philos rocks bootcamp',
+    resave: false,
+    saveUninitialized: false
+  }));
+
+  let dirPath = path.join(__dirname,'../public');
+  app.use(express.static(dirPath));
+};
