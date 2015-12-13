e7e5a97dd8377656f6cec5aa6caa8b1332faae0f
davy engone <davy@philos.io>
2015-12-13 14:19:37 +0100

    moved app conguration into on the server side

--- server/config.js
+++ server/config.js
@@ -6,6 +6,11 @@ const config = {
   PORT : process.env.PORT || 8000,
   DB : process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/bootcamp',
   URL : process.env.NODE_ENV === 'production'? 'https://admission.philos.io': 'http://localhost:8000',
+  AppConfig: {
+    currentUserUrl: process.env.NODE_ENV === 'production'? '/users/me': 'http://localhost:8000/users/me',
+    registerUrl: process.env.NODE_ENV === 'production'? '/users/register': 'http://localhost:8000/users/register',
+    githubUrl: process.env.NODE_ENV === 'production'? '/auth/github': 'http://localhost:8000/auth/github'
+  },
   email:{
     from: 'Philos Bootcamp', // sender address
     to: (email) => email,
