3f7f664126d630b45aa359b6597f65aa02cf5e9d
davy engone <davy@philos.io>
2015-12-13 07:33:49 +0100

    added babel preset for es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,30 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-	entry: './index.js',
-	output:{
-		filename: 'build.js',
-    path: 'dist'
-	},
-
- 	module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel"
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /\.html$/,
-        loader: "raw"
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      },
-      {
-        test: "\.jpg$",
-        loader: "file"
-      },
-      {
-        test: "\.png$",
-        loader: "url"
-      }]
+  devtool: 'eval-source-map',
+  entry: './public/index',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: "bundle.js"
   },
-
-  devServer: {
-    contentBase: "./",
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devServer:{
+    contentBase: './public'
   }
 };
