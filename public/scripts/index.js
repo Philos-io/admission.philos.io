7830317aba45371f755f23a94bec28c5e56c702f
davy engone <davy@philos.io>
2015-12-13 14:29:17 +0100

    finished refactored into es6

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,26 @@
-(function(module){
-	'use strict';
+import angular, {module, bootstrap} from 'angular';
+import 'angular-route';
 
-	angular.module('admission.philos.io', ['ngRoute'])
 
-  document.addEventListener('DOMContentLoaded', function(){
-    angular.bootstrap(document.body, ['admission.philos.io'], {strictDI: true});
-  });
-})();
+import routes                 from '../components/routes';
+import userFactory            from '../components/user-factory';
+import AppController          from '../components/app-controller';
+import philosAdmission        from '../components/main-directive';
+import headerDirective        from '../components/header-directive';
+import disabledForm           from '../components/disable-forms-directive';
+import ConfirmationController from '../components/confirmation-controller';
+import AppConfig              from '../components/app-config';
+
+module('admission.philos.io', ['ngRoute'])
+  .config(routes)
+  .controller('AppController', AppController)
+  .controller('ConfirmationController', ConfirmationController)
+  .directive('disabledForm', disabledForm)
+  .directive('philosHeader', headerDirective)
+  .directive('philosAdmission', philosAdmission)
+  .factory('userFactory', userFactory)
+  .value('AppConfig', AppConfig);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  bootstrap(document.body, ['admission.philos.io'], {strictDI: true});
+});
