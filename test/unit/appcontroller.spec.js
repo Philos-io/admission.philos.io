9ae58510e0dc5c9149e5419dacb37fbc10c21077
davy engone <davy@philos.io>
2015-12-14 23:16:17 +0100

    added testing strategy

---
+++ test/unit/appcontroller.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('AppController', function(){
+
+  beforeEach(module('admission.philos.io'));
+
+  let $controller;
+
+  it('Dummy test', function(){
+    expect(5).toEqual(5);
+  })
+});
