c1cee19cc31bbf564f8d17da4827016ef8f4888d
davy engone <davy@philos.io>
2015-12-16 06:08:07 +0100

    prod scripts

--- public/components/routes.js
+++ public/components/routes.js
@@ -1,20 +1,20 @@
 export default function Configuration($routeProvider){
   $routeProvider
-  .when('/', {
-    templateUrl: './views/welcome.html',
-    controller: 'AppController',
-    controllerAs: 'vm'
-  })
-  .when('/register', {
-    templateUrl: './views/welcome.html',
-    controller: 'AppController',
-    controllerAs: 'vm'
-  })
-  .when('/confirmation', {
-    templateUrl: './views/confirmation.html',
-    controller: 'ConfirmationController',
-    controllerAs: 'vm'
-  });
+     .when('/', {
+       templateUrl: './views/welcome.html',
+       controller: 'AppController',
+       controllerAs: 'vm'
+     })
+     .when('/register', {
+       templateUrl: './views/welcome.html',
+       controller: 'AppController',
+       controllerAs: 'vm'
+     })
+     .when('/confirmation', {
+       templateUrl: './views/confirmation.html',
+       controller: 'ConfirmationController',
+       controllerAs: 'vm'
+     });
 }
 
 Configuration.$inject = ['$routeProvider'];
