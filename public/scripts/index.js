2e0242b5875df975713e95173ab2d1caf4e6e44e
davy engone <davy@philos.io>
2015-12-28 23:07:25 +0100

    removed debugger from index

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,6 @@
 import angular, {module, bootstrap} from 'angular';
 import 'angular-route';
 
-
 import routes                 from '../components/routes';
 import userFactory            from '../components/userFactory';
 import AppController          from '../components/appController';
@@ -9,7 +8,10 @@ import philosAdmission        from '../components/mainDirective';
 import headerDirective        from '../components/headerDirective';
 import disabledForm           from '../components/disabledFormsDirective';
 import ConfirmationController from '../components/confirmationController';
+import confirmationDirective  from '../components/confirmationDirective';
+import shelldirective         from '../components/shellDirective';
 import AppConfig              from '../components/appConfig';
+
 
 module('admission.philos.io', ['ngRoute'])
   .config(routes)
@@ -18,6 +20,8 @@ module('admission.philos.io', ['ngRoute'])
   .directive('disabledForm', disabledForm)
   .directive('philosHeader', headerDirective)
   .directive('philosAdmission', philosAdmission)
+  .directive('shell', shelldirective)
+  .directive('confirmation', confirmationDirective)
   .factory('userFactory', userFactory)
   .value('AppConfig', AppConfig);
 
