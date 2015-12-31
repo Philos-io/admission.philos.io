a7ff5887295e3fbb356f46b3337254975207c57a
davy engone <davy@philos.io>
2015-12-31 23:08:33 +0100

    added mockup tests for appcontroller

--- test/unit/appcontroller.spec.js
+++ test/unit/appcontroller.spec.js
@@ -4,9 +4,15 @@ describe('AppController', function(){
 
   beforeEach(module('admission.philos.io'));
 
-  let $controller;
+  let $controller, $scope;
 
-  it('Dummy test', function(){
-    expect(5).toEqual(5);
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+
+    let ctrl = $controller('AppController', {$scope: $scope});
+  }));
+
+  it('View model associated with the Appcontroller', function(){
+
   })
 });
