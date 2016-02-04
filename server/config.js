93c7455c196a60f2e5b8f458dbc4c4305a468761
davy engone <davy@philos.io>
2016-02-04 05:47:39 +0100

    refactored config

--- server/config.js
+++ server/config.js
@@ -1,15 +1,17 @@
+const prod = process.env.NODE_ENV === 'production';
+
 const config = {
-  GITHUB_CLIENT_ID : process.env.NODE_ENV === 'production'? process.env.GITHUB_CLIENT_ID: "f344a987c3d87a83b193",
-  GITHUB_CLIENT_SECRET : process.env.NODE_ENV === 'production'? process.env.GITHUB_CLIENT_SECRET : "7a814ebd607af91f1e36392ba79d097815bb5082",
-  APPLICATION_ID : process.env.NODE_ENV === 'production'? process.env.APPLICATION_ID: "Mixvf1k0dyzRjwP6iMaVBoA6EBNiUrFPDr8hU1d1",
-  JAVASCRIPT_KEY : process.env.NODE_ENV === 'production'? process.env.JAVASCRIPT_KEY: "wCos1ljBvek1nrksJdfyDRerYhrORYayvTh9W5Dl",
+  GITHUB_CLIENT_ID : prod? process.env.GITHUB_CLIENT_ID: "f344a987c3d87a83b193",
+  GITHUB_CLIENT_SECRET : prod? process.env.GITHUB_CLIENT_SECRET : "7a814ebd607af91f1e36392ba79d097815bb5082",
+  APPLICATION_ID : prod? process.env.APPLICATION_ID: "Mixvf1k0dyzRjwP6iMaVBoA6EBNiUrFPDr8hU1d1",
+  JAVASCRIPT_KEY : prod? process.env.JAVASCRIPT_KEY: "wCos1ljBvek1nrksJdfyDRerYhrORYayvTh9W5Dl",
   PORT : process.env.PORT || 8000,
   DB : process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/bootcamp',
-  URL : process.env.NODE_ENV === 'production'? 'https://admission.philos.io': 'http://localhost:8000',
+  URL : prod? 'https://admission.philos.io': 'http://localhost:8000',
   AppConfig: {
-    currentUserUrl: process.env.NODE_ENV === 'production'? '/users/me': 'http://localhost:8000/users/me',
-    registerUrl: process.env.NODE_ENV === 'production'? '/users/register': 'http://localhost:8000/users/register',
-    githubUrl: process.env.NODE_ENV === 'production'? '/auth/github': 'http://localhost:8000/auth/github'
+    currentUserUrl: prod? '/users/me': 'http://localhost:8000/users/me',
+    registerUrl: prod? '/users/register': 'http://localhost:8000/users/register',
+    githubUrl: prod? '/auth/github': 'http://localhost:8000/auth/github'
   },
   email:{
     from: 'Philos Bootcamp', // sender address
